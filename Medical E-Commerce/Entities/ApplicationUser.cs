cb09adabf5fa40dc02da7c73486ca80e4852f72b
omar <omarfaroq2003@gmail.com>
2025-03-26 01:15:15 +0200

    add images to user

--- Medical E-Commerce/Entities/ApplicationUser.cs
+++ Medical E-Commerce/Entities/ApplicationUser.cs
@@ -6,4 +6,6 @@ public class ApplicationUser : IdentityUser
 {
     public string UserFullName { get; set; } = string.Empty;
     public string UserAddress { get; set; } = string.Empty;
+    public int? ImageId { get; set; }
+    public Image? Image { get; set; } = default!;
 }
---
+++ Medical E-Commerce/Entities/Image.cs
@@ -0,0 +1,11 @@
+﻿namespace Medical_E_Commerce.Entities;
+
+public class Image
+{
+    public Guid Id { get; set; } = Guid.CreateVersion7();
+    public string FileName { get; set; } = string.Empty;
+    public string ContentType { get; set; } = string.Empty;
+    public string FileExtenstions { get; set; } = string.Empty;
+    public string UserId { get; set; } = string.Empty;
+    public ApplicationUser User { get; set; } = default!;
+}
