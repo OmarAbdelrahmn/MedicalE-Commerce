9cae669615e63113c49bd91056ed03228bed067d
omar <omarfaroq2003@gmail.com>
2025-03-25 02:02:02 +0200

    adding dbcontext

---
+++ Medical E-Commerce/Persistence/ApplicationDbcontext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Medical_E_Commerce.Persistence;
+
+public class ApplicationDbcontext(DbContextOptions<ApplicationDbcontext> options) : IdentityDbContext(options)
+{
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        var cascadeFKs = modelBuilder.Model.GetEntityTypes()
+            .SelectMany(t => t.GetForeignKeys())
+            .Where(fk => !fk.IsOwnership && fk.DeleteBehavior == DeleteBehavior.Cascade);
+
+        foreach (var fk in cascadeFKs)
+            fk.DeleteBehavior = DeleteBehavior.Restrict;
+
+
+        base.OnModelCreating(modelBuilder);
+
+    }
+}
