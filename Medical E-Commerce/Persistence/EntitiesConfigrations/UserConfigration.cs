6b6556d60ff360d004e1c94f86da95d28ec82445
omar <omarfaroq2003@gmail.com>
2025-03-25 02:33:55 +0200

    adding user entityconfigurations

---
+++ Medical E-Commerce/Persistence/EntitiesConfigrations/UserConfigration.cs
@@ -0,0 +1,20 @@
+﻿using Medical_E_Commerce.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Medical_E_Commerce.Persistence.EntitiesConfigrations;
+
+public class UserConfigration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.Property(c => c.UserFullName)
+            .HasMaxLength(50)
+            .IsRequired();
+        
+
+        builder.Property(c => c.UserAddress)
+            .HasMaxLength(50)
+            .IsRequired();
+    }
+}
