c625accbdd729191b94d7a8d17a50484b50d0b3b
omar <omarfaroq2003@gmail.com>
2025-03-26 01:37:55 +0200

    add roles

---
+++ Medical E-Commerce/Persistence/EntitiesConfigrations/RolesConfigrations.cs
@@ -0,0 +1,36 @@
+﻿using Medical_E_Commerce.Abstractions.Consts;
+using Medical_E_Commerce.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Medical_E_Commerce.Persistence.EntitiesConfigrations;
+
+public class RolesConfigrations : IEntityTypeConfiguration<ApplicationRoles>
+{
+    public void Configure(EntityTypeBuilder<ApplicationRoles> builder)
+    {
+
+        builder.HasData(
+            [
+                new ApplicationRoles
+                {
+                    Id = DefaultRoles.AdminRoleId,
+                    Name = DefaultRoles.Admin,
+                    ConcurrencyStamp = DefaultRoles.AdminRoleConcurrencyStamp,
+                    NormalizedName = DefaultRoles.Admin.ToUpper(),
+                    IsDefault = false,
+                    IsDeleted = false
+                },
+                new ApplicationRoles
+                {
+                    Id = DefaultRoles.MemberRoleId,
+                    Name = DefaultRoles.Member,
+                    ConcurrencyStamp = DefaultRoles.MemberRoleConcurrencyStamp,
+                    NormalizedName = DefaultRoles.Member.ToUpper(),
+                    IsDefault = true,
+                    IsDeleted = false
+                }
+            ]
+        );
+    }
+}
