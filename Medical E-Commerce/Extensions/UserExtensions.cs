ad294e42990f6b6d566b1ad893f7c88f073b74bd
omar <omarfaroq2003@gmail.com>
2025-03-27 01:15:16 +0200

    add user extension method GetbyId

---
+++ Medical E-Commerce/Extensions/UserExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace Medical_E_Commerce.Extensions;
+
+public static class UserExtensions
+{
+    public static string? GetUserId(this ClaimsPrincipal user)
+    {
+        return user.FindFirstValue(ClaimTypes.NameIdentifier);
+    }
+}
