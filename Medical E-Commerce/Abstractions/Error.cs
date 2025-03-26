49fe52d61d907e1eeccdab65dcee04aa78be6e1d
omar <omarfaroq2003@gmail.com>
2025-03-27 00:17:05 +0200

    add Result pattern and exceptionhandler

---
+++ Medical E-Commerce/Abstractions/Error.cs
@@ -0,0 +1,6 @@
+﻿namespace Medical_E_Commerce.Abstractions;
+
+public record Error(string Code, string Description, int? StatuesCode)
+{
+    public static Error non => new(string.Empty, string.Empty, null);
+}
