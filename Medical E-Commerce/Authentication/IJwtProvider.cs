4b5272994487cd5710ec897ebd83be691e99c9de
omar <omarfaroq2003@gmail.com>
2025-03-27 01:30:24 +0200

    add jwt provider

---
+++ Medical E-Commerce/Authentication/IJwtProvider.cs
@@ -0,0 +1,10 @@
+﻿using Medical_E_Commerce.Entities;
+
+namespace Medical_E_Commerce.Authentication;
+
+public interface IJwtProvider
+{
+    (string Token, int Expiry) GenerateToken(ApplicationUser user, IEnumerable<string> Roles);
+
+    string? ValidateToken(string token);
+}
---
+++ Medical E-Commerce/Authentication/JwtProvider.cs
@@ -0,0 +1,68 @@
+﻿using Medical_E_Commerce.Entities;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text.Json;
+using System.Text;
+namespace Medical_E_Commerce.Authentication;
+
+public class JwtProvider(IOptions<JwtOptions> options) : IJwtProvider
+{
+    private readonly JwtOptions options = options.Value;
+
+    public (string Token, int Expiry) GenerateToken(ApplicationUser user, IEnumerable<string> Roles)
+    {
+        Claim[] claims = [
+            new (JwtRegisteredClaimNames.Sub, user.Id),
+            new (JwtRegisteredClaimNames.Email, user.Email!),
+            new (JwtRegisteredClaimNames.GivenName, user.UserFullName),
+            new (nameof(user.UserAddress) , user.UserAddress),
+            new (JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new (nameof(Roles),JsonSerializer.Serialize(Roles),JsonClaimValueTypes.JsonArray)
+            ];
+
+        var SymmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(options.Key));
+
+        var signingCredentials = new SigningCredentials(SymmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+
+
+        var token = new JwtSecurityToken(
+            issuer: options.Issuer,
+            audience: options.Audience,
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(options.ExpiryIn),
+            signingCredentials: signingCredentials
+        );
+
+        return (Token: new JwtSecurityTokenHandler().WriteToken(token), Expiry: options.ExpiryIn);
+    }
+
+    public string? ValidateToken(string token)
+    {
+        var tokenhandler = new JwtSecurityTokenHandler();
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(options.Key));
+
+        try
+        {
+            tokenhandler.ValidateToken(token, new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero,
+                IssuerSigningKey = key
+            }, out SecurityToken validatedToken);
+
+            var jwtToken = (JwtSecurityToken)validatedToken;
+
+            return jwtToken.Claims.First(claim => claim.Type == JwtRegisteredClaimNames.Sub).Value;
+        }
+        catch
+        {
+            return null;
+
+        }
+    }
+}
