f60b0e97de690eb75e0ec8a8a837f098208db74a
omar <omarfaroq2003@gmail.com>
2025-03-28 00:15:47 +0200

    add sign in service

--- Medical E-Commerce/Abstractions/Errors/UserErrors.cs
+++ Medical E-Commerce/Abstractions/Errors/UserErrors.cs
@@ -4,5 +4,8 @@ public class UserErrors
 {
     public static readonly Error InvalidCredentials = new("User.InvalidCredentials", "Invalid Email/Password", StatusCodes.Status401Unauthorized);
     public static readonly Error UserAlreayExists = new("User.UserAlreadyExists", "User with this Email exists", StatusCodes.Status401Unauthorized);
+    public static readonly Error Disableuser = new("User.UserIsDisable", "This user is disable", StatusCodes.Status401Unauthorized);
+    public static readonly Error EmailNotConfirmed = new("User.EmailNotConfirmed", "This is not confirmed ", StatusCodes.Status401Unauthorized);
+    public static readonly Error userLockedout = new("User.userLockedout", "user Lockedout , contact the admin", StatusCodes.Status401Unauthorized);
 
 }
