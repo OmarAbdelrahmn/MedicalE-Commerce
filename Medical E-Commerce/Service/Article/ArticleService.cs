011b47ed497320490cf9f4a1f2448616353d632d
omar <omarfaroq2003@gmail.com>
2025-03-29 01:16:21 +0200

    add write services

--- Medical E-Commerce/Service/Article/ArticleService.cs
+++ Medical E-Commerce/Service/Article/ArticleService.cs
@@ -3,47 +3,78 @@ using Medical_E_Commerce.Abstractions;
 using Medical_E_Commerce.Abstractions.Errors;
 using Medical_E_Commerce.Contracts.Article;
 using Medical_E_Commerce.Persistence;
+using Medical_E_Commerce.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Medical_E_Commerce.Service.Article;
 
 public class ArticleService(ApplicationDbcontext context) : IArticleService
 {
+    public async Task<Result<ArticleResponse>> AddAsynce(ArticleRequest request, CancellationToken cancellationToken = default)
+    {
+        var Article = request.Adapt<Entities.Article>();
+
+        await context.Articles.AddAsync(Article);
+        await context.SaveChangesAsync();
+
+        return Result.Success(Article.Adapt<ArticleResponse>());
+
+    }
+
     public async Task<Result<IEnumerable<ArticleResponse>>> GetAll(CancellationToken cancellationToken = default)
     {
-        var Diseases = await context.Articles
+        var articles = await context.Articles
             .ProjectToType<ArticleResponse>()
             .AsNoTracking()
             .ToListAsync(cancellationToken);
 
-        return Result.Success<IEnumerable<ArticleResponse>>(Diseases);
+        return Result.Success<IEnumerable<ArticleResponse>>(articles);
     }
 
     public async Task<Result<ArticleResponse>> GetByIdAsynce(int id, CancellationToken cancellationToken = default)
     {
-        var Diseases = await context.Articles
+        var articles = await context.Articles
             .Where(x => x.Id == id)
             .ProjectToType<ArticleResponse>()
             .AsNoTracking()
             .SingleOrDefaultAsync(cancellationToken);
 
-        if (Diseases is null)
+        if (articles is null)
             return Result.Failure<ArticleResponse>(ArticleErrors.ArticleNotNound);
 
-        return Result.Success(Diseases);
+        return Result.Success(articles);
     }
 
     public async Task<Result<ArticleResponse>> GetByNameAsynce(string Name, CancellationToken cancellationToken = default)
     {
-        var Diseases = await context.Articles
+        var articles = await context.Articles
                    .Where(x => x.Name.Contains(Name))
                    .ProjectToType<ArticleResponse>()
                    .AsNoTracking()
                    .SingleOrDefaultAsync(cancellationToken);
 
-        if (Diseases is null)
+        if (articles is null)
             return Result.Failure<ArticleResponse>(ArticleErrors.ArticleNotNound);
 
-        return Result.Success(Diseases);
+        return Result.Success(articles);
+    }
+
+    public async Task<Result<ArticleResponse>> UpdateAsynce(int id, ArticleRequest request, CancellationToken cancellationToken = default)
+    {
+        var article = await context.Articles
+            .Where(x => x.Id == id)
+            .SingleOrDefaultAsync(cancellationToken);
+
+        if (article is null)
+            return Result.Failure<ArticleResponse>(ArticleErrors.ArticleNotNound);
+
+        article.Name = request.Name;
+        article.Description = request.Description;
+
+        context.Articles.Update(article);
+
+        await context.SaveChangesAsync(cancellationToken);
+
+        return Result.Success(article.Adapt<ArticleResponse>());
     }
 }
--- Medical E-Commerce/Service/Article/IArticleService.cs
+++ Medical E-Commerce/Service/Article/IArticleService.cs
@@ -7,5 +7,7 @@ public interface IArticleService
 {
     Task<Result<ArticleResponse>> GetByIdAsynce(int id, CancellationToken cancellationToken = default);
     Task<Result<ArticleResponse>> GetByNameAsynce(string Name, CancellationToken cancellationToken = default);
+    Task<Result<ArticleResponse>> AddAsynce(ArticleRequest request, CancellationToken cancellationToken = default);
+    Task<Result<ArticleResponse>> UpdateAsynce(int id ,ArticleRequest request, CancellationToken cancellationToken = default);
     Task<Result<IEnumerable<ArticleResponse>>> GetAll(CancellationToken cancellationToken = default);
 }
