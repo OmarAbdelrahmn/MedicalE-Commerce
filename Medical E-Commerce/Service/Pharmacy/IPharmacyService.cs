b8468a2f2d5771c7eea95e52f8c5981f0dad98fa
omar <omarfaroq2003@gmail.com>
2025-03-29 16:20:55 +0200

    add writes service

--- Medical E-Commerce/Service/Pharmacy/IPharmacyService.cs
+++ Medical E-Commerce/Service/Pharmacy/IPharmacyService.cs
@@ -7,5 +7,7 @@ public interface IPharmacyService
 {
     Task<Result<PharmacyResponse>> GetByNameAsync(string name);
     Task<Result<PharmacyResponse>> GetByIdAsync(int Id);
+    Task<Result<PharmacyResponse>> AddAsync(PharmacyRequest request);
+    Task<Result<PharmacyResponse>> UpdateAsync(int Id , PharmacyRequest request);
     Task<Result<IEnumerable<PharmacyResponse>>> GetAllAsync();
 }
