4c23efe61e4edb57604e6bb0a9da7bc797a1130a
omar <omarfaroq2003@gmail.com>
2025-04-16 17:18:10 +0200

    add notification body

--- Medical E-Commerce/Service/Notifications/NotinficationService.cs
+++ Medical E-Commerce/Service/Notifications/NotinficationService.cs
@@ -1,5 +1,55 @@
-﻿namespace Medical_E_Commerce.Service.Notifications;
+﻿
+using Medical_E_Commerce.Helpers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
-public class NotinficationService : INotinficationService 
+namespace Medical_E_Commerce.Service.Notifications;
+
+public class NotinficationService(
+    ApplicationDbcontext dbcontext,
+    UserManager<ApplicationUser> userManager ,
+    IHttpContextAccessor httpContextAccessor,
+    IEmailSender emailSender) : INotinficationService
 {
+    private readonly ApplicationDbcontext dbcontext = dbcontext;
+    private readonly UserManager<ApplicationUser> userManager = userManager;
+    private readonly IHttpContextAccessor httpContextAccessor = httpContextAccessor;
+    private readonly IEmailSender emailSender = emailSender;
+
+    
+       public async Task SendNewPollsNotification()
+        {
+        IEnumerable<Entities.Pharmacy> Pharmacy = [];
+
+
+        Pharmacy = await dbcontext.Pharmacies
+                .Where(x => x.Items.Any())
+                .AsNoTracking()
+                .Take(3)
+                .ToListAsync();
+        
+
+        //TODO: Select members only
+        var users = await userManager.Users.ToListAsync();
+
+        var origin = httpContextAccessor.HttpContext?.Request.Headers.Origin;
+
+        foreach (var Pharmacyi in Pharmacy)
+        {
+            foreach (var user in users)
+            {
+                var placeholders = new Dictionary<string, string>
+                {
+                    { "{{name}}", user.UserFullName },
+                    { "{{pollTill}}", Pharmacyi.Name},
+                    { "{{endDate}}",$"{DateTime.UtcNow.AddDays(3)} "},
+                    { "{{url}}", $"{origin}/pharmacy/start/{Pharmacyi.Id}" }
+                };
+
+                var body = EmailBodyBuilder.GenerateEmailBody("Care-Capsole Notification", placeholders);
+
+                await emailSender.SendEmailAsync(user.Email!, $"📣 Care-Capsole: Pharmacy - {Pharmacyi.Name}", body);
+            }
+        }
+    }
 }
