b8aee4ee4c4f4a0b93191c46198c03e68c57f269
omar <omarfaroq2003@gmail.com>
2025-04-16 14:37:39 +0200

    add endpoints

---
+++ Medical E-Commerce/Controllers/OrdersController.cs
@@ -0,0 +1,32 @@
+﻿using Medical_E_Commerce.Abstractions;
+using Medical_E_Commerce.Service.Order;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Medical_E_Commerce.Controllers;
+[Route("[controller]")]
+[ApiController]
+public class OrdersController(IOrderService service) : ControllerBase
+{
+    private readonly IOrderService service = service;
+
+    [HttpGet("user/{UserId}")]
+    public async Task<IActionResult> GetOrders(string UserId)
+    {
+       var orders = await service.GetUserId(UserId);
+       
+        return orders.IsSuccess
+            ? Ok(orders.Value)
+            : orders.ToProblem();
+    }
+    
+    [HttpGet("pharmacy/{PharmacyId}")]
+    public async Task<IActionResult> GetOrders(int PharmacyId)
+    {
+       var orders = await service.GetpharmacyId(PharmacyId);
+       
+        return orders.IsSuccess
+            ? Ok(orders.Value)
+            : orders.ToProblem();
+    }
+}
--- Medical E-Commerce/Service/Order/IOrderService.cs
+++ Medical E-Commerce/Service/Order/IOrderService.cs
@@ -6,7 +6,7 @@ namespace Medical_E_Commerce.Service.Order;
 
 public interface IOrderService
 {
-    Task<Result<IEnumerable<OrderResopnse>>> GetUserId(int Userid);
+    Task<Result<IEnumerable<OrderResopnse>>> GetUserId(string Userid);
     Task<Result<IEnumerable<OrderResopnse>>> GetpharmacyId(int PharmacyId);
 
 }
